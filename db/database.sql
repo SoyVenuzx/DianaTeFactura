86333dd0a508f89d91e63d1d27b8a5eb0a840cef
ealvarez <ealvarez@ni.thedataage.com>
2023-05-01 02:51:59 -0600

    BD Terminada & Consultas

--- db/database.sql
+++ db/database.sql
@@ -1,122 +1,209 @@
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'DianaTeFactura')
-BEGIN
-  DROP DATABASE DianaTeFactura;
-END
+DROP DATABASE DianaTeFactura;
 
 CREATE DATABASE DianaTeFactura;
 
-create table productos (
-	IDProductos int primary key auto_increment,
-	NombreProducto varchar(150) not null,
-	Precio decimal not null,
-	CantidadInventario int not null
+-- Productos
+CREATE TABLE productos (
+	IDProducto INT PRIMARY KEY AUTO_INCREMENT,
+	NombreProducto VARCHAR(150) NOT NULL,
+	Precio DECIMAL(10, 2) NOT NULL CHECK (Precio >= 0),
+	CantidadInventario INT NOT NULL CHECK (CantidadInventario >= 0),
+	UnidadMedida VARCHAR(20)
 );
 
-create table tipo_productos (
-	IDTipoProductos int primary key auto_increment,
-	descripcion varchar(100) not null,
-	foreign key (IDTipoProductos) references productos(IDProductos)
+CREATE TABLE tipo_productos (
+	IDTipo INT PRIMARY KEY AUTO_INCREMENT,
+	Descripcion VARCHAR(100) NOT NULL,
+	IDProducto INT,
+	FOREIGN KEY (IDProducto) REFERENCES productos(IDProducto)
 );
 
-create table ventas (
-	IDVentas int primary key auto_increment,
-	Fecha datetime default current_timestamp(),
-	CantidadVendida int not null,
-	TotalVentas decimal not null,
-	IDProductos int,
-	foreign key (IDProductos) references productos(IDProductos)
+-- Contacto
+CREATE TABLE contactos (
+	idContacto int PRIMARY KEY AUTO_INCREMENT,
+	tipoContacto varchar(50) NOT NULL,
+	valor varchar(100) NOT NULL
 );
 
-create table tipo_ventas (
-	IDTipoVenta  int primary key auto_increment,
-	descripcion varchar(100) not null,
-	foreign key (IDTipoVenta) references ventas(IDVentas)
+-- Clientes
+CREATE TABLE clientes (
+	idCliente int PRIMARY KEY AUTO_INCREMENT,
+	nombreCliente varchar(50) NOT NULL,
+	apellidoCliente varchar(50) NOT NULL,
+	idContacto int,
+	FOREIGN KEY (idContacto) REFERENCES contactos(idContacto)
 );
 
-create table proveedores (
-	IDProveedor int primary key auto_increment,
-	NombreProveedor varchar(50) not null,
-	Email varchar(50) not null,
-	Contacto int not null
-);
-
-create table compras (
-	IDCompra int primary key auto_increment,
-	Fecha datetime default current_timestamp(), 
-	CantidadComprada int not null,
-	TotalCompra decimal not null,
-	IDProveedor int,
-	foreign key (IDProveedor) references proveedores(IDProveedor)
-);
-
-create table tipo_compras (
-	IDTipoCompra int primary key auto_increment,
-	descripcion varchar(100) not null,
-	foreign key (IDTipoCompra) references compras(IDCompra)
-);
-
-create table inventario (
-	IDInventario int primary key auto_increment,
-	IDProductos int,
-	CantidadDisponible int not null,
-	FechaRegistro datetime default current_timestamp(),
-	descripcion varchar(100) not null,
-	cantidadEntrda int not null,
-	cantidadSalida int not null,
-	precioVenta decimal not null,
-	Total decimal not null,
-	foreign key (IDProductos) references productos(IDProductos)
-);
-
-create table clientes (
-	IDCliente int primary key auto_increment,
-	NombreCliente varchar(50) not null,
-	ApellidoCliente varchar(50) not null
-);
-
-create table contacto (
-	IDContacto int primary key auto_increment,
-	descripcion varchar(50) not null,
-	valor varchar(100) not null,
-	IDCliente int,
+-- Ventas
+CREATE TABLE ventas (
+	IDVenta INT PRIMARY KEY AUTO_INCREMENT,
+	Fecha DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	CantidadVendida INT NOT NULL,
+	TotalVentas DECIMAL NOT NULL,
+	IDProducto INT,
+	IDCliente INT,
+	FOREIGN KEY (IDProducto) REFERENCES productos(IDProducto),
 	FOREIGN KEY (IDCliente) REFERENCES clientes(IDCliente)
 );
 
-create table facturas (
-	IDFacturas int primary key auto_increment,
-	IDCliente int, 
-	FechaEmision datetime default current_timestamp(),
-	TotalFactura decimal not null,
-	foreign key (IDCliente) references clientes(IDCliente)
+CREATE TABLE tipo_ventas (
+	IDTipoVenta INT PRIMARY KEY AUTO_INCREMENT,
+	descripcion VARCHAR(100) NOT NULL,
+	FOREIGN KEY (IDTipoVenta) REFERENCES ventas(IDVenta)
 );
 
-create table detalles_factura (
-	IDdetalle_factura int primary key auto_increment,
-	IDFacturas int, 
-	IDProducto int,
-	Cantidad int not null,
-	PrecioUnitario decimal not null,
-	TotalDetalle decimal not null,
-	descripcion varchar(150) not null
+-- Proveedores
+CREATE TABLE proveedores (
+	idProveedor int PRIMARY KEY AUTO_INCREMENT,
+	nombreProveedor varchar(50) NOT NULL,
+	idContacto int,
+	FOREIGN KEY (idContacto) REFERENCES contactos(idContacto)
 );
 
-create table usuariosDiana (
-	IDUsuario int primary key auto_increment,
-	nombreUsuario varchar(25) not null,
-	apellidoUsuario varchar(25) not null,
-	posicion varchar(30) not null
+-- Mapeo contactos
+CREATE TABLE mapeo_contacto (
+	idMapeo int PRIMARY KEY AUTO_INCREMENT,
+	idContacto int NOT NULL,
+	idProveedor int,
+	idCliente int,
+	FOREIGN KEY (idContacto) REFERENCES contactos(idContacto),
+	FOREIGN KEY (idProveedor) REFERENCES proveedores(idProveedor),
+	FOREIGN KEY (idCliente) REFERENCES clientes(idCliente)
 );
 
--- INSERT INTO clientes (NombreCliente, ApellidoCliente)
--- VALUES 
--- ('Juan', 'Pérez'),
--- ('María', 'García');
+-- Compras 
+CREATE TABLE compras (
+	IDCompra INT PRIMARY KEY AUTO_INCREMENT,
+	Fecha DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	CantidadComprada INT NOT NULL,
+	TotalCompra DECIMAL(10, 2) NOT NULL,
+	IDProveedor INT,
+	FOREIGN KEY (IDProveedor) REFERENCES proveedores(IDProveedor)
+);
 
--- SELECT * FROM clientes;
+CREATE TABLE tipo_compras (
+	IDTipoCompra INT PRIMARY KEY AUTO_INCREMENT,
+	descripcion VARCHAR(100) NOT NULL
+);
 
--- INSERT INTO contacto (descripcion, valor, IDCliente)
--- VALUES 
--- ('Teléfono', '1234567890', 1),
--- ('Dirección', 'Calle Falsa 123', 2);
+CREATE TABLE compras_tipo_compras (
+	IDCompra INT,
+	IDTipoCompra INT,
+	PRIMARY KEY (IDCompra, IDTipoCompra),
+	FOREIGN KEY (IDCompra) REFERENCES compras(IDCompra),
+	FOREIGN KEY (IDTipoCompra) REFERENCES tipo_compras(IDTipoCompra)
+);
 
--- SELECT * FROM contacto;
+-- Inventario
+CREATE TABLE inventario (
+	IDInventario INT PRIMARY KEY AUTO_INCREMENT,
+	IDProducto INT,
+	CantidadDisponible INT NOT NULL,
+	FechaRegistro DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	descripcion VARCHAR(100) NOT NULL,
+	cantidadEntrada INT NOT NULL,
+	cantidadSalida INT NOT NULL,
+	precioVenta DECIMAL(10, 2) NOT NULL,
+	Total DECIMAL(10, 2) NOT NULL,
+	FOREIGN KEY (IDProducto) REFERENCES productos(IDProducto)
+);
+
+-- Facturas
+CREATE TABLE factura (
+	idFactura INT PRIMARY KEY AUTO_INCREMENT,
+	idCliente INT,
+	fechaEmision DATETIME DEFAULT CURRENT_TIMESTAMP,
+	estado VARCHAR(50) NOT NULL DEFAULT 'pendiente',
+	total DECIMAL NOT NULL,
+	FOREIGN KEY (idCliente) REFERENCES clientes(idCliente)
+);
+
+CREATE TABLE detalle_factura (
+	idDetalleFactura INT PRIMARY KEY AUTO_INCREMENT,
+	idFactura INT,
+	idProductoVendido INT,
+	cantidad INT NOT NULL,
+	precioUnitario DECIMAL NOT NULL,
+	totalDetalle DECIMAL NOT NULL,
+	descripcion VARCHAR(150) NOT NULL,
+	FOREIGN KEY (idFactura) REFERENCES factura(idFactura),
+	FOREIGN KEY (idProductoVendido) REFERENCES productos(idProducto)
+);
+
+-- Usuarios
+CREATE TABLE usuarios (
+	IDUsuario int PRIMARY KEY AUTO_INCREMENT,
+	nombreUsuario varchar(25) NOT NULL,
+	contrasenia varchar(100) NOT NULL
+);
+
+CREATE TABLE informacion_usuario (
+	IDUsuario int PRIMARY KEY AUTO_INCREMENT,
+	nombre varchar(25) NOT NULL,
+	apellido varchar(25) NOT NULL,
+	rol varchar(30) NOT NULL,
+	correoElectronico varchar(50) NOT NULL,
+	telefono varchar(20) NOT NULL,
+	FOREIGN KEY (IDUsuario) REFERENCES usuarios(IDUsuario)
+);
+
+------------------------------------------------------------------------------
+
+-- Ejemplos para crear proveedores/clientes/productos:
+-- Insertando contacto
+INSERT INTO
+	contactos (tipoContacto, valor)
+VALUES
+	('email', 'proveedor3@ejemplo.com');
+
+-- Insertando proveedor
+INSERT INTO
+	proveedores (nombreProveedor, idContacto)
+VALUES
+	('Proveedor 1', LAST_INSERT_ID());
+
+-- Insertar mapeo
+INSERT INTO
+	mapeo_contacto (idContacto, idProveedor)
+VALUES
+	(LAST_INSERT_ID(), LAST_INSERT_ID());
+
+------------------------------------------------------------------------------
+-- Insertar un cliente
+-- Insertar un contacto
+INSERT INTO
+	contactos (tipoContacto, valor)
+VALUES
+	('Email', 'cliente4@ejemplo.com');
+
+-- Insertar un cliente y obtener su id
+INSERT INTO
+	clientes (nombreCliente, apellidoCliente, idContacto)
+VALUES
+	('Antonio', 'Rodriguez', LAST_INSERT_ID());
+
+-- Enlazar el contacto y el cliente mediante la tabla mapeo_contacto
+INSERT INTO
+	mapeo_contacto (idContacto, idCliente)
+VALUES
+	(LAST_INSERT_ID(), LAST_INSERT_ID());
+
+------------------------------------------------------------------------------
+-- Insertando productos
+INSERT INTO
+	productos (
+		NombreProducto,
+		Precio,
+		CantidadInventario,
+		UnidadMedida
+	)
+VALUES
+	('Producto 1', 10.50, 100, 'pieza'),
+	('Producto 2', 8.75, 50, 'kg');
+
+-- Insertando tipos de productos
+INSERT INTO
+	tipo_productos (Descripcion, IDProducto)
+VALUES
+	('Tipo 1', 1),
+	('Tipo 2', 2);
